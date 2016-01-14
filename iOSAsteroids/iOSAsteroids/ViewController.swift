8114614a1dabdcfdcdb1061d11e8b203f5e86c23
MathiasBulte <bulte.mathias@gmail.com>
2016-01-14 19:34:16 +0100

    Project upload

---
+++ iOSAsteroids/iOSAsteroids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSAsteroids
+//
+//  Created by Mathias on 10/12/2015.
+//  Copyright © 2015 Mathias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
